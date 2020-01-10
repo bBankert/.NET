fd6ce1c91cf7b1da404443caab3f7d0d4da0af15
bBanker <fireninja171@gmail.com>
2020-01-10 09:38:49 -0500

    final touches

--- authentication/AuthenticationTesting/Models/User.cs
+++ authentication/AuthenticationTesting/Models/User.cs
@@ -5,9 +5,14 @@ namespace AuthenticationTesting.Models{
     public class User{
         public int ID {get;set;}
 
-        public string Name {get;set;}  
-              
+        
+        [MinLength(1),Required]
+        public string Name {get;set;}
+
+        [MinLength(1),Required]      
         public string Username {get;set;}
+
+        [MinLength(1),Required]
         public string Password {get;set;}
         [DataType(DataType.Date)]
         public DateTime DateOfBirth {get;set;}
--- authentication/AuthenticationTesting/Pages/Users/Index.cshtml.cs
+++ authentication/AuthenticationTesting/Pages/Users/Index.cshtml.cs
@@ -9,7 +9,7 @@ using AuthenticationTesting.Data;
 using AuthenticationTesting.Models;
 using System.Text;
 using System.Security.Cryptography;
-using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Http.Extensions; 
 
 namespace AuthenticationTesting.Pages.Users
 {
@@ -41,6 +41,7 @@ namespace AuthenticationTesting.Pages.Users
         public IList<User> User { get;set; }
 
 
+        //query params
         [BindProperty(SupportsGet = true)]
         public string username { get;set;}
 
@@ -50,12 +51,46 @@ namespace AuthenticationTesting.Pages.Users
 
         public async Task OnGetAsync()
         {
+
+            //get username & password query params
+            string username_query = Request.Query["username"];
+            string password_query = Request.Query["password"];
+            //change to restful url
+            string new_path = "users/"+username_query+'/'+password_query;
+            string current_path = HttpContext.Request.GetEncodedUrl();
+            //makes sure there are query params and if so, change to routing
+            if((!string.IsNullOrEmpty(username) || !string.IsNullOrEmpty(password))
+            && current_path.Contains('?')){
+                //immediate redirect to proper path if query params exist
+                Response.Redirect(new_path);                
+            }
+
+
+
+
+
             //search for specific user
             var users = from u in _context.User select u;
             if(!string.IsNullOrEmpty(username) && !string.IsNullOrEmpty(password)){
                 users = users.Where(u => u.Username.Equals(username) && u.Password.Equals(password));
+                
             }
+
+            //get eveything for testing
+            if(username.Equals("admin") && password.Equals("123")){
+                users = _context.User;
+            }
+
+
+            //if no users from login redirect to login page
+            if(users.Count() == 0){
+                Response.Redirect("../Index");
+            }
+
             User = await users.ToListAsync();
+
+
+
         }
     }
 }
